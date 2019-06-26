ef17e3ffc77c98a7d4e82b84e21db121e4460b8f
dennomwas <githinji.mwangi@gmail.com>
2019-06-26 15:04:04 +0300

    [Ft] Add maps component
    - Add search component
    - add top navigation component
    - add side navigation component
    - delete home component

--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { SearchComponentComponent } from './search.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('SearchComponentComponent', () => {
+  let component: SearchComponentComponent;
+  let fixture: ComponentFixture<SearchComponentComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ SearchComponentComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(SearchComponentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
