e41d4de4a7805242ff90295ebc0afefc70647fcd
dennomwas <githinji.mwangi@gmail.com>
2019-06-26 14:56:15 +0300

    [Ft] create add map components
    - Change folder naming

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { SearchComponent } from './components/search/search.component';
+import {  MapsComponent} from './components/maps/maps.component';
+import { AddmapComponent } from './components/addmap/addmap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TopNavComponent,
+    MainNavComponent,
+    SideNavComponent,
+    SearchComponent,
+    MapsComponent,
+    AddmapComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/addmap/addmap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addmap',
+  templateUrl: './addmap.component.html',
+  styleUrls: ['./addmap.component.css']
+})
+export class AddmapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
